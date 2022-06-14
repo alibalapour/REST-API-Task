513bad46517144b89d2aea2cfdac0103c9811d3d
Ali <alibalapour93.ab@gmail.com>
2022-06-14 12:31:53 -0700

    Add get request & Add Item and History classes to models.py

--- API/main.py
+++ API/main.py
@@ -1,4 +1,21 @@
 from typing import List
 from uuid import UUID, uuid4
 from fastapi import FastAPI, HTTPException
+from models import Item
 
+app = FastAPI()
+
+db: List[Item] = [
+    Item(
+        key = 1,
+        value = 'sfsfd'
+    )
+]
+
+@app.get("/")
+async def read_root():
+    return {"Hello There :)"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
--- API/models.py
+++ API/models.py
@@ -1,4 +1,24 @@
-from typing import Optional, List
+from typing import Optional, List, Any, Union
 from uuid import UUID, uuid4
 from pydantic import BaseModel
 from enum import Enum
+import datetime
+
+
+class History:
+    version: str
+    value: Union[int, str]  # value of corresponded version
+    date: str
+
+    def __init__(self, value) -> None:
+        self.version = '1.0'
+        self.value = value
+        self.date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+
+class Item(BaseModel):
+    id: Optional[UUID] = uuid4()
+    key: Union[int, str]
+    value: Union[int, str]   # current value
+    # history: Optional[List[History]]
+
