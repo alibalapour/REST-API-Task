64b4529ec9b8567781d52560604e4123d62051b4
Ali <alibalapour93.ab@gmail.com>
2022-06-14 12:03:31 -0700

    Create general structure of the project

---
+++ API/main.py
@@ -0,0 +1,4 @@
+from typing import List
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
+
---
+++ API/models.py
@@ -0,0 +1,4 @@
+from typing import Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
