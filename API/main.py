5d07fbde7ae0242cf53cc1892e57394f239569b7
Ali <alibalapour93.ab@gmail.com>
2022-06-14 23:12:49 -0700

    Add delete_user as a DELETE request

--- API/main.py
+++ API/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from uuid import UUID, uuid4
 from fastapi import FastAPI, HTTPException
 from models import Item, History
@@ -29,9 +29,22 @@ async def fetch_items():
 
 
 @app.post("/api/v1/users")
-async def set_items(item:Item):
+async def set_item(item:Item):
     item.history = [History(value = item.value,
      date = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))]
     db.append(item)
     return {'id': item.id}
 date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+@app.delete("/api/v1/users/{key}")
+async def delete_item(key:Union[str, int]):
+    for item in db:
+        print(type(item.key), type(key))
+        if item.key == key or str(item.key) == str(key):      # item.key: string & key: int
+            db.remove(item)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f'item with key: {key} was not found'
+    )
